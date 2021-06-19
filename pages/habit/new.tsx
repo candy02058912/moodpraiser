a44b3ae4e78f4540657f9e1eb36c678ba5e1ceab
Candy Tsai <candy02058912@gmail.com>
2021-06-19 22:53:35 +0800

    finish create habit skeleton

--- pages/habit/new.tsx
+++ pages/habit/new.tsx
@@ -1,13 +1,32 @@
 import { Button } from "@chakra-ui/button";
 import { FormControl } from "@chakra-ui/form-control";
 import { Input } from "@chakra-ui/input";
-import { Box, Center, Heading, VStack } from "@chakra-ui/layout";
+import {
+  Box,
+  Center,
+  Heading,
+  LinkBox,
+  LinkOverlay,
+  VStack,
+} from "@chakra-ui/layout";
 import { Collapse } from "@chakra-ui/transition";
-import { useState } from "react";
+import Link from "next/link";
+import { useRef, useState } from "react";
 import Default from "../../components/layouts/Default/Default";
 
 const CreateHabit = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [step, setStep] = useState(1);
+  const [isCopied, setIsCopied] = useState(false);
+  const linkRef = useRef<HTMLInputElement>(null);
+  const copyToClipboard = () => {
+    linkRef.current!.select();
+    document.execCommand("copy");
+    setIsCopied(true);
+    setStep(step + 1);
+  };
+  const handleAddHabit = () => {
+    setStep(step + 1);
+  };
   return (
     <Default>
       <Center>
@@ -23,9 +42,9 @@ const CreateHabit = () => {
             <FormControl id="habit">
               <Input />
             </FormControl>
-            <Button onClick={() => setIsOpen(true)}>Add habit</Button>
+            <Button onClick={handleAddHabit}>Add habit</Button>
           </VStack>
-          <Collapse in={isOpen}>
+          <Collapse in={step >= 2}>
             <VStack mt={8} spacing={4} align="flex-start">
               <Heading as="h3" size="md">
                 Step 2
@@ -33,9 +52,20 @@ const CreateHabit = () => {
               <Heading as="h2" size="lg">
                 Invite praiser
               </Heading>
-              <Input value="https://hi" />
-              <Button>Copy invite link</Button>
+              <Input ref={linkRef} value="https://hi" readOnly />
+              <Button onClick={copyToClipboard}>
+                {isCopied ? "Link copied" : "Copy invite link"}
+              </Button>
             </VStack>
+          </Collapse>
+          <Collapse in={step >= 3}>
+            <LinkBox mt={8}>
+              <Link href="/" passHref>
+                <LinkOverlay>
+                  <Button>Back to dashboard</Button>
+                </LinkOverlay>
+              </Link>
+            </LinkBox>
           </Collapse>
         </Box>
       </Center>
