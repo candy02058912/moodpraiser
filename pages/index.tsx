a2fa946f75e58330b48bfd5bd749683b0f950663
Candy Tsai <candy02058912@gmail.com>
2021-06-19 21:39:40 +0800

    extract layout to Default

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
-import Head from "next/head";
 import { useUser } from "@auth0/nextjs-auth0";
-import { Box, Center, Container } from "@chakra-ui/layout";
+import { Center } from "@chakra-ui/layout";
 import HomePage from "../components/templates/Homepage/Homepage";
 import Dashboard from "../components/templates/Dashboard/Dashboard";
 import { Spinner } from "@chakra-ui/spinner";
-import Header from "../components/modules/Header/Header";
+import Default from "../components/layouts/Default/Default";
 
 const Main = () => {
   const { user, error, isLoading } = useUser();
@@ -26,15 +25,8 @@ const Main = () => {
 
 export default function Home() {
   return (
-    <Box>
-      <Head>
-        <title>Moodpraiser</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Container maxW="container.lg">
-        <Header />
-        <Main />
-      </Container>
-    </Box>
+    <Default>
+      <Main />
+    </Default>
   );
 }
