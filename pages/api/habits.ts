5515a6582859ea0a1b0c79ff96b4341865cd3e06
Candy Tsai <candy02058912@gmail.com>
2021-06-20 00:56:33 +0800

    add createHabit api

--- pages/api/habits.ts
+++ pages/api/habits.ts
@@ -1,15 +1,24 @@
 import { withApiAuthRequired, getSession, Session } from "@auth0/nextjs-auth0";
-import { queryHabitsByUID } from "../../server/api/habits";
+import { createHabit, queryHabitsByUID } from "../../server/api/habits";
 
 export default withApiAuthRequired(async function handler(req, res) {
   const { user } = getSession(req, res) as Session;
-  if (req.method === "GET") {
-    const habits = await queryHabitsByUID(user.uid);
-    res.json({ habits });
-  } else {
-    res.status(405).json({
-      error: "method_not_allowed",
-      description: `${req.method} method not allowed`,
-    });
+  switch (req.method) {
+    case "GET":
+      const habits = await queryHabitsByUID(user.uid);
+      res.json({ habits });
+      break;
+    case "POST":
+      const {
+        body: { name },
+      } = req;
+      const resp = await createHabit(name as string, user.uid);
+      res.json({ id: resp.inserted_hashes[0] });
+      break;
+    default:
+      res.status(405).json({
+        error: "method_not_allowed",
+        description: `${req.method} method not allowed`,
+      });
   }
 });
--- server/api/habits.ts
+++ server/api/habits.ts
@@ -1,10 +1,29 @@
 import axios from "axios";
 import SQLString from "sqlstring";
+import { DBUser } from "../../common/types";
 
 export const queryHabitsByUID = (uid: string) => {
   const data = JSON.stringify({
     operation: "sql",
     sql: SQLString.format(`SELECT * FROM dev.habits WHERE owner = ?`, [uid]),
+  });
+
+  return axios({ data })
+    .then(function (response) {
+      return response.data;
+    })
+    .catch(function (error) {
+      console.log("error", error);
+    });
+};
+
+export const createHabit = (name: string, uid: string) => {
+  const data = JSON.stringify({
+    operation: "sql",
+    sql: SQLString.format(
+      `INSERT INTO dev.habits (name, owner) VALUES (?, ?)`,
+      [name, uid]
+    ),
   });
 
   return axios({ data })
